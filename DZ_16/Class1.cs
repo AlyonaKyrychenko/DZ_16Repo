fdaab959ef890144e6fb755ce7872e695f368d69
AlyonaKyrychenko <74854299+AlyonaKyrychenko@users.noreply.github.com>
2020-12-21 16:14:02 +0200

    Added DZ_16 project

---
+++ DZ_16/Class1.cs
@@ -0,0 +1,21 @@
+﻿namespace DZ_16
+{
+    /// <summary>
+    /// This is for multiplying.
+    /// </summary>
+    public class Class1
+    {
+        private DelegateShow handler = Program.Show;
+
+        /// <summary>
+        /// Multiply first with second.
+        /// </summary>
+        /// <param name="x">First multiplier.</param>
+        /// <param name="y">Second multiplier.</param>
+        /// <returns>Result of multiply.</returns>
+        public static int Pow(int x, int y)
+        {
+            return x * y;
+        }
+    }
+}
---
+++ DZ_16/Program.cs
@@ -0,0 +1,53 @@
+﻿namespace DZ_16
+{
+    using System;
+
+    /// <summary>
+    /// This deligate for multiply two numbers.
+    /// </summary>
+    /// <param name="firstMultiplier">This fisrt multiplier.</param>
+    /// <param name="secondMultiplier">This second multiplier.</param>
+    /// <returns>Result of multiply.</returns>
+    public delegate int DelegatePow(int firstMultiplier, int secondMultiplier);
+
+    /// <summary>
+    /// This id for devide result of multiply on some number.
+    /// </summary>
+    /// <param name="devider">This is devider.</param>
+    /// <returns>Result of devide.</returns>
+    public delegate bool DelegateDevide(int devider);
+
+    /// <summary>
+    /// This delegate for showing rezult of programm implementation.
+    /// </summary>
+    /// <param name="res">Result of programm implementation.</param>
+    public delegate void DelegateShow(bool res);
+
+    /// <summary>
+    /// This id start of the programm.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// This is starting method.
+        /// </summary>
+        /// <param name="args">Some args.</param>
+        public static void Main(string[] args)
+        {
+            DelegatePow del = Class1.Pow;
+            Class2 ob = new Class2();
+            DelegateDevide calc = ob.Calc(del, 3, 10);
+            bool rez = calc.Invoke(6);
+            Show(rez);
+        }
+
+        /// <summary>
+        /// This is for showing result.
+        /// </summary>
+        /// <param name="res">This is for result.</param>
+        public static void Show(bool res)
+        {
+            Console.WriteLine(res.ToString());
+        }
+    }
+}
