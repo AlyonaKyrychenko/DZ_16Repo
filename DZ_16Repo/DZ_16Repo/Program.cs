b36b981837e4e0a98a40e8960c9c9b07a5e1d3d9
AlyonaKyrychenko <74854299+AlyonaKyrychenko@users.noreply.github.com>
2020-12-21 16:12:06 +0200

    Added empty project

---
+++ DZ_16Repo/DZ_16Repo/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DZ_16Repo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
